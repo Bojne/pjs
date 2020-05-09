0c794dbdd1beaeae9baaf72e818aa158cebbf26c
bojne <bojne.john@gmail.com>
2020-05-09 17:10:31 +0800

    new

---
+++ index.js
@@ -0,0 +1,25 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import './styles.scss';
+
+const destination = document.getElementById("root");
+
+class App extends React.Component {
+
+    constructor(props) {
+      super(props);
+      this.state = {
+        emoji:['🎉', '🧘‍♂️','🏃‍♀️','💃','💅','🙆🏻‍♀️','👼','👸','👩🏼‍💻','🤟','🙌','🤯','🥳','📚','✨','🍦','🤸','🏖','🎊','🌏'][Math.floor(Math.random()*16)],
+      };
+    }
+    render() {
+      return (
+          <div class = 'layout'>
+            <h2>Hello!</h2>
+            <iframe src="https://www.openprocessing.org/sketch/885111/embed/" width="400" height="300"></iframe>
+          </div>  
+      )  
+    }
+}
+  
+ReactDOM.render(<App />, destination);
